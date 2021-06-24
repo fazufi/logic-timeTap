a10c6b4a1d83ecec3e23d5d2414c13768bc27fb4
fazufi <fazufi10@gmail.com>
2021-06-24 22:59:05 +0700

    mouth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    // day: [],
+    time1: [
+      { hour: 23, minute: 0 },
+      { hour: null, minute: null },
+      { hour: null, minute: null },
+    ],
+    time2: [
+      { hour: null, minute: null },
+      { hour: null, minute: null },
+      { hour: null, minute: null },
+    ],
+  };
+
+  tambah = (e) => {
+    e.preventDefault();
+    const { time1, time2 } = this.state;
+    const i = time1.findIndex((f) => f.hour == null);
+    const i2 = time2.findIndex((f) => f.hour == null);
+    if (time1[2].hour === null) {
+      let lastMinute1 = i && time1[i - 1].minute;
+      time1[i] = { hour: time1[0].hour, minute: lastMinute1 + 10 };
+      this.setState({ time1 });
+    } else {
+      let lastMinute2 = null;
+      time2[0].hour === null
+        ? (lastMinute2 = time1[2].minute)
+        : (lastMinute2 = time2[i2-1].minute);
+
+      time2[i2] = { hour: time1[0].hour, minute: lastMinute2 + 10 };
+      this.setState({ time2 });
+
+    }
+ 
+    if (time2[2].hour !== null) {
+      const lastHour = time1[0].hour;
+      this.setState({
+        time1: [
+          { hour: lastHour + 1, minute: 0 },
+          { hour: null, minute: null },
+          { hour: null, minute: null },
+        ], time2:  [
+          { hour: null, minute: null },
+          { hour: null, minute: null },
+          { hour: null, minute: null },
+        ]
+      });
+    
+    }
+console.log(time2[2].hour);
+    // let lastHour = 0;
+    // time1[].hour === null
+    //   ? (lastHour = time1[i - 1].hour)
+    //   : (lastHour = time1[0].hour);
+    // let lastMinute = 0;
+    // time2[2].hour === null
+    //   ? (lastMinute = time1[i - 1].minute)
+    //   : (lastMinute = 0);
+    // if (time2[2].hour === null) {
+    //   time1[i] = { hour: lastHour, minute: lastMinute + 10 };
+    //   this.setState({ time1 });
+    //   console.log("IIFFFFFFF");
+    // } else {
+    //   this.setState({
+    //     time1: [
+    //       { hour: lastHour + 1, minute: 0 },
+    //       { hour: null, minute: null },
+    //       { hour: null, minute: null },
+    //       { hour: null, minute: null },
+    //       { hour: null, minute: null },
+    //       { hour: null, minute: null },
+    //     ],
+    //   });
+    //   console.log("ELSEEEE");
+    // }
+    // console.log(lastHour + 1);
+
+    // console.log(time2[2].hour === null);
+  };
+
+  render() {
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Col>
+              {this.state.time1.map((t, i) => (
+                <p>
+                  {i + 1}. {t.hour} {t.hour !== null && ": "}
+                  {t.minute}
+                </p>
+              ))}
+            </Col>
+
+            <Col>
+              {this.state.time2.map((t, i) => (
+                <p>
+                  {i + 4}. {t.hour} {t.hour !== null && ": "}
+                  {t.minute}
+                </p>
+              ))}
+            </Col>
+          </Row>
+        </Container>
+        <button onClick={this.tambah}>tambah</button>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/App2.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default class App2 extends Component {
+  state = {
+    time: [],
+  };
+  add = () => {
+    const { time } = this.state;
+    if (time.length) {
+      const last = time[time.length - 1];
+      let hour = last.hour;
+      let minute = last.minute;
+      if (last.minute === 50) {
+        minute = 0;
+        hour = hour + 1;
+      } else {
+        minute = minute + 10;
+      }
+      time.push({ hour, minute });
+    } else {
+      time.push({ hour: 1, minute: 0 });
+    }
+    this.setState({ time });
+  };
+  render() {
+    const { time } = this.state;
+    console.log("\n");
+    const jumlahpanel = 6;
+    let perenam = [];
+    let persatu = [];
+    for (const [i, t] of time.entries()) {
+      t.index = i+1
+      if (i % jumlahpanel === 0 && i !== 0) {
+        perenam.push(persatu);
+        persatu = [];
+        persatu.push(t);
+      } else {
+        persatu.push(t);
+      }
+    }
+    console.log(persatu.length, persatu);
+    return (
+      <div className="container">
+        <button onClick={(e) => this.add()}>Add</button>
+        <div className="row">
+          <div className="col-md-6">
+            col1
+            {persatu.length
+              ? persatu.map((d, i) => {
+                  return i < 3 ? <div>{JSON.stringify(d)}</div> : null;
+                })
+              : null}
+          </div>
+          <div className="col-md-6">
+            col2
+            {persatu.length
+              ? persatu.map((d, i) => {
+                  return i >= 3 ? <div>{JSON.stringify(d)}</div> : null;
+                })
+              : null}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
